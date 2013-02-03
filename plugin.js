efb7b70fcb629df9d2f073b07bdaebcf438875b1
cadorn <christoph@christophdorn.com>
2013-02-03 01:56:56 -0800

    more formats

--- plugin.js
+++ plugin.js
@@ -16,7 +16,13 @@ exports.for = function(API, plugin) {
                 locations.zip = locator.url;
             } else
             if (/(\.tgz|\.tar\.gz)$/.test(locator.url)) {
-                locations.tar = locator.url;
+                locations.gzip = locator.url;
+            } else
+            if (/(\.tar\.bz2)$/.test(locator.url)) {
+                locations.bzip = locator.url;
+            } else
+            if (/(\.7z)$/.test(locator.url)) {
+                locations["7zip"] = locator.url;
             }
             return (type)?locations[type]:locations;
         }
